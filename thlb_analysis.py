30187766e11e257d23c52677ef03303d187adc25
MoezLabiadh <60794110+MoezLabiadh@users.noreply.github.com>
2020-02-24 11:24:54 -0800

    Update thlb_analysis.py

--- thlb_analysis.py
+++ thlb_analysis.py
@@ -1,5 +1,5 @@
 '''
-This script generates generate Timber harvest metrics, mainly areas and volumes, for a specific area of interest (AOI). The AOI can be either a Timber supply Area, an Operating area or other ( FN claimed area,?). Tha main steps are:
+This script generates Timber harvest metrics, mainly areas and volumes, for a specific area of interest (AOI). The AOI can be either a Timber supply Area, an Operating area or other ( FN claimed area,?). Tha main steps are:
     1) Clip the input layers to the AOI extent (VRI, OGMA, THLB,...). Other input layers can be added depending on the scope of the analysis
     2) Perform a spatial overlay of the clipped input layers
     3) Add and populate fields based on defined rules: mature timber, merchantability, areas with harvest constraints..
